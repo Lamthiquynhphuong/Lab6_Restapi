40361aacd1247ef03894612a432a1e3892e25fd6
Lamthiquynhphuong <lamquynhphuong7@gmail.com>
2024-07-28 22:08:41 +0700

    first commit

---
+++ app/src/test/java/edu/select/SvrResponseSelect.java
@@ -0,0 +1,16 @@
+package edu.select;
+
+import com.example.lab6.SanPham;
+
+public class SvrResponseSelect {
+    private SanPham[] products;
+    private String message;
+
+    public SanPham[] getProducts() {
+        return products;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
